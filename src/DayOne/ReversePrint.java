c706a0e3dc91b7ddb34e757a0086427c95752ddf
Shailesh-Tagadghar <shaileshtagadghar13@gmail.com>
2023-10-08 10:20:21 +0530

    reverse / palindrome string

--- src/DayOne/ReversePrint.java
+++ src/DayOne/ReversePrint.java
@@ -1,18 +1,38 @@
 package DayOne;
 
+import java.util.Scanner;
+
 public class ReversePrint {
 	public static void main(String[] args) {
-		String str = "Python";
-		System.out.println(str.charAt(5));
-		System.out.println(str.charAt(4));
-		System.out.println(str.charAt(3));
-		System.out.println(str.charAt(2));
-		System.out.println(str.charAt(1));
-		System.out.println(str.charAt(0));
+//		String str = "Python";
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter String ");
+		String str = sc.next();
+//		System.out.println(str.charAt(5));
+//		System.out.println(str.charAt(4));
+//		System.out.println(str.charAt(3));
+//		System.out.println(str.charAt(2));
+//		System.out.println(str.charAt(1));
+//		System.out.println(str.charAt(0));
 		
-		  char[] resultarray = str.toCharArray();
-		  for (int i = resultarray.length - 1; i >= 0; i--)
-		            System.out.print(resultarray[i]);
+//		  char[] resultarray = str.toCharArray();
+//		  for (int i = resultarray.length - 1; i >= 0; i--)
+//		            System.out.print(resultarray[i]);
+		
+		String reverse = "";
+		for(int i = str.length()-1;i>=0;i--) {
+			reverse = reverse + str.charAt(i);
+//			System.out.println(str.charAt(i));
+		}
+		
+		System.out.println(reverse);
+		
+		if(str.equals(reverse)) {
+			System.out.println(str + " is Palindrome");
+		}
+		else {
+			System.out.println(str + " is not Palindrome");
+		}
 		}
 	}
 
