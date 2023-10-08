d1f1927def164901e5e04c35626b84d213b68bee
Shailesh-Tagadghar <shaileshtagadghar13@gmail.com>
2023-10-08 11:41:38 +0530

    String Methods

---
+++ src/DayTwo/OtherMethods.java
@@ -0,0 +1,19 @@
+package DayTwo;
+
+public class OtherMethods {
+	public static void main(String[] args) {
+		
+		String str = "Welcome";
+		//substring --> string_name.substring(start_index);
+		String sub = str.substring(3); //come
+		System.out.println(sub);
+		
+		//substring --> string_name.substring(start_index,end_index);
+		sub = str.substring(1,4); //elc
+		System.out.println(sub);
+		
+		//split method
+		
+		
+	}
+}
